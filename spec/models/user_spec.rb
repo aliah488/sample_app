d394a7cd220d6e2c7377ff5fd2f1d2c82b814622
Ali <aliah488@gmail.com>
2013-04-06 08:38:24 -0400

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -27,9 +27,17 @@ describe User do
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:authenticate) }
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) }
+
 
   it { should be_valid }
 
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
 
   describe "when name is not present" do
      before { @user.name = " " }
@@ -77,7 +85,7 @@ describe User do
   end
 
   describe "when password is not present" do
-    before { @user.password = @user.password_confirmation = " " }
+    before { @user.password = " " }
       it { should_not be_valid }
   end
 
